7f27e8cb150ae1d023ec7de62873e13bc93e3594
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2024-12-02 12:40:24 +0200

    init

---
+++ CSRS/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+open System.Windows.Forms
+
+[<EntryPoint>]
+let main argv = 
+
+    let form = new Form(Text = "F#-SUCKS", Width = 400, Height = 300)
+
+    let label = new Label(Text = "Hello world", AutoSize=true, Top = 100, Left = 150)
+
+    form.Controls.Add(label)
+
+    Application.Run(form)
+
+    0
+
