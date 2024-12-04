c15b66520a3bb5a02dfe4423a10878ef0356e74f
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2024-12-04 21:06:47 +0200

    add: implement LoginForm module and integrate it into Program.fs

--- CSRS/Program.fs
+++ CSRS/Program.fs
@@ -3,45 +3,8 @@ open System.Drawing
 open System.Windows.Forms
 open System.IO
 
-open Utils
+open CSRS
 
-let LoginForm onClickHandler =
-    let form = new Form(Text = "Login Form", Width = 400, Height = 250)
-    let lblUsername =
-        new Label(
-            Text = "Username:",
-            Width = 200,
-            Height = 30,
-            TextAlign = ContentAlignment.MiddleCenter,
-            Font = new Font("Arial", 12.0F, FontStyle.Bold),
-            Top = 50,
-            Left = 100
-        )
-
-    let txtUsername =
-        new TextBox(Width = 200, Height = 30, Font = new Font("Arial", 12.0F, FontStyle.Regular), Top = 90, Left = 100)
-
-    let btnLogin =
-        new Button(
-            Text = "Login",
-            Width = 100,
-            Height = 40,
-            Font = new Font("Arial", 12.0F, FontStyle.Bold),
-            Top = 140,
-            Left = 150
-        )
-
-    let loginAction _ =
-        if String.IsNullOrWhiteSpace(txtUsername.Text) then
-            MessageBox.Show("Please enter a username.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-            |> ignore
-        else
-            onClickHandler txtUsername.Text
-            form.Close()
-
-    form.Controls.AddRange([| lblUsername; txtUsername; btnLogin |])
-    btnLogin.Click.Add(loginAction)
-    form
 
 
 // Run the LoginForm
@@ -49,8 +12,7 @@ let LoginForm onClickHandler =
 let main _ =
 
     Application.EnableVisualStyles()
-    let form = LoginForm (fun username -> 
-
+    let form = LoginForm.initForm(fun username -> 
         MessageBox.Show($"Welcome, {username}!", "Welcome", MessageBoxButtons.OK, MessageBoxIcon.Information)
         |> ignore
     )
