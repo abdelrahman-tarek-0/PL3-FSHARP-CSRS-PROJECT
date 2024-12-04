f4ff5a5e44e7f9e30b5329b3d2b8675ef6f9af8a
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2024-12-05 00:11:20 +0200

    add: implement random string generator and improve ticket formatting in Utils module

--- CSRS/Utils.fs
+++ CSRS/Utils.fs
@@ -4,29 +4,40 @@ open System
 open System.IO
 
 module TicketOperations =
-    let DateAandtTime () = 
+    let DateAandtTime () =
         let now = System.DateTime.Now
         now.ToString("yyyy-MM-dd HH:mm:ss")
-   
+
+    let randomString () =
+        let random = new Random()
+        let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        let stringChars = Array.zeroCreate<char> 8
+
+        for i = 0 to 7 do
+            stringChars.[i] <- chars.[random.Next(chars.Length)]
+
+        new string (stringChars)
+
     let addticket (id: int) (username: string) (date: string) (seat: int) (filePath: string) =
-        let newLine = sprintf "%d,%s,%s,%d" id username date seat
+        let newLine = sprintf "%d\n%s\n%s\n%d" id username date seat
+
         try
-            File.AppendAllText(filePath, newLine + Environment.NewLine)
-            id=id+1
-            printfn "Successfully added: %s" newLine
-        with
-             | :? IOException as ex ->
-                printfn "An error occurred while writing to the file: %s" ex.Message
+            File.AppendAllText(filePath, newLine)
+            1
+        with :? IOException as ex ->
+            printfn "An error occurred while writing to the file: %s" ex.Message
+            0
 
 
     let readticket filePath =
         try
             let lines = File.ReadLines(filePath)
             let mutable linesList = []
+
             for line in lines do
-                linesList <- line::linesList
+                linesList <- line :: linesList
+
             linesList
-        with
-            | :? IOException as ex ->
-                printfn "An error occurred while writing to the file: %s" ex.Message
-                []
+        with :? IOException as ex ->
+            printfn "An error occurred while writing to the file: %s" ex.Message
+            []
