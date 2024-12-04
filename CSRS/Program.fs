f3c39946698989ee60ba938adb080a67c39327f3
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2024-12-05 01:00:24 +0200

    add: integrate SeatsForm into main program flow and handle seat selection

--- CSRS/Program.fs
+++ CSRS/Program.fs
@@ -7,15 +7,37 @@ open CSRS
 
 
 
+
 // Run the LoginForm
 [<EntryPoint>]
 let main _ =
-
     Application.EnableVisualStyles()
-    let form = LoginForm.initForm(fun username -> 
-        MessageBox.Show($"Welcome, {username}!", "Welcome", MessageBoxButtons.OK, MessageBoxIcon.Information)
-        |> ignore
-    )
+
+    let form =
+        LoginForm.initForm (fun username ->
+            MessageBox.Show($"Welcome, {username}!", "Welcome", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            |> ignore
+
+            let seatsForm =
+                SeatsForm.initForm (fun seat ->
+                        MessageBox.Show(
+                            $"You have selected seat {seat}.",
+                            "Seat Selected",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information
+                        )
+                        |> ignore
+
+                        SeatsStore.editSeatStatus seat SeatsStore.SeatStatus.R |> ignore
+                        
+
+                    )
+
+            seatsForm.Show()
+
+
+        )
+
     Application.Run(form)
 
     0
